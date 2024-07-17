54d0a38db8294c53836db19c15c7d6f322b8bc7c
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-07-16 21:00:32 -0300

    Configuração para AWS ECS

---
+++ src/main/java/com/myorg/AluraRdsStack.java
@@ -0,0 +1,75 @@
+package com.myorg;
+
+import java.util.Collections;
+
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnParameter;
+import software.amazon.awscdk.SecretValue;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.ISecurityGroup;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
+import software.amazon.awscdk.services.ec2.SubnetSelection;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.Credentials;
+import software.amazon.awscdk.services.rds.CredentialsFromUsernameOptions;
+import software.amazon.awscdk.services.rds.DatabaseInstance;
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
+import software.amazon.awscdk.services.rds.MySqlInstanceEngineProps;
+import software.amazon.awscdk.services.rds.MysqlEngineVersion;
+import software.constructs.Construct;
+
+
+public class AluraRdsStack extends Stack {
+    public AluraRdsStack(final Construct scope, final String id, final Vpc vpc) {
+        this(scope, id, null, vpc);
+    }
+
+    public AluraRdsStack(final Construct scope, final String id, final StackProps props, final Vpc vpc) {
+        super(scope, id, props);
+
+        CfnParameter senha = CfnParameter.Builder.create(this, "senha")
+        		.type("String")
+        		.description("Senha do database orders-ms")
+        		.build();
+        
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+        
+        DatabaseInstance database = DatabaseInstance.Builder
+                .create( this, "Rds-orders")
+                .instanceIdentifier("alura-aws-orders-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_8_0)
+                        .build()))
+                .vpc(vpc)
+                .credentials(Credentials.fromUsername("admin",
+                        CredentialsFromUsernameOptions.builder()
+                                .password(SecretValue.unsafePlainText(senha.getValueAsString()))
+                                .build()))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+        
+        CfnOutput.Builder.create(this, "orders-db-endpoint")
+        .exportName("orders-db-endpoint")
+        .value(database.getDbInstanceEndpointAddress())
+        .build();
+        
+        CfnOutput.Builder.create(this, "orders-db-senha")
+        .exportName("orders-db-senha")
+        .value(senha.getValueAsString())
+        .build();
+        
+    }
+}
